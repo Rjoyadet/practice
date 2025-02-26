402e327c9cd6ca4ab10885f49c1037091cc7ebb0
Rjoyadet <57276368+Rjoyadet@users.noreply.github.com>
2025-02-26 11:24:44 -0600

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,23 @@
+
+Print("Greetings lovely bags of mostly water")
+    awkward = input("Did you understand the reference")
+    if no == 'no' or 'um'
+        print("Well... this is awkward")
+    else:
+        print("It is from Star Trek TNG")
+	print('Hello. I am Ricks chatbot. call me Roger')
+	name = input('What is your name?: ')
+	print('Hello',name, ',Nice to meet you')
+
+    print("Do you watch any science fiction shows?")
+    if no == 'no' or 'um'
+        print("well.. this is awkard")
+    else: 
+        print("Cool, I am getting excited")
+    print("Do you like older or newer")
+    if older == "older" or "old"
+        print("I do too, ironic isn't it?")
+    else
+        print("Me, not so much, ironic isn't it")
+    print("Well, I better be going, its nice talking to you.")
+    print("See you later. ")
